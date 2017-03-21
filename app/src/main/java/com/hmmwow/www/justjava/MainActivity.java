03a300047eefe7524c64d3c2e7c02c765fb78cdf
renzmallari <renzmallari777@yahoo.com>
2017-03-21 04:58:04 -0400

    version 1 released

--- app/src/main/java/com/hmmwow/www/justjava/MainActivity.java
+++ app/src/main/java/com/hmmwow/www/justjava/MainActivity.java
@@ -7,9 +7,13 @@ package com.hmmwow.www.justjava;
  * package com.example.android.justjava;
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -39,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
     /* INCREMENT */
     public void increment(View view) {
-        quantity = quantity + 1;
+        if (quantity <= 100){
+            quantity = quantity + 1;
+        }
         displayQuantity(quantity);
 
     }
@@ -57,25 +63,31 @@ public class MainActivity extends AppCompatActivity {
     /*
     * THIS METHOD WILL GIVE TXT TO THE SCREEN
      */
-    private void displayText(String message){
-        TextView orderSummaryTextView  = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView .setText(message);
+    private String displayText(String message){
+//        TextView orderSummaryTextView  = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView .setText(message);
+
+        String thanks = displayTextThankyou();
+
+        return message + "\n" +thanks;
     }
 
     /*
     *     THANK YOU TEXT VIEW
      */
-    public void displayTextThankyou(){
-        TextView thankYouMessage = (TextView) findViewById(R.id.thank_you_text_view);
-        thankYouMessage.setText("Thank You!");
+    public String displayTextThankyou(){
+//        TextView thankYouMessage = (TextView) findViewById(R.id.thank_you_text_view);
+//        thankYouMessage.setText("Thank You!");
+
+        return getString(R.string.thank_you);
     }
 
     /**
      * will calculate the total price
      * @ return total price
      */
-    public double calculatePrice(int count, double price){
-        double totalPrice = count * price;
+    public double calculatePrice(int count, double price, int wCream, int choco){
+        double totalPrice = count * (price + wCream + choco);
         return totalPrice;
     }
 
@@ -83,25 +95,64 @@ public class MainActivity extends AppCompatActivity {
      *  Summary of the order
      */
 
-    private String createOrderSummary(){
+    private String createOrderSummary(Boolean whippedCream, Boolean chocolate, String name){
+        int wCream = (whippedCream)?  1:0;
+        int choco = (chocolate)? 3:0;
+
+
         if (quantity < 1){
-            return "Name: Jayson Mallari \nQuantity: "+ quantity + "\nTotal: Its Free";
+            return getString(R.string.order_summary_name,name)
+                    + "\nAdd whipped cream: " + whippedCream
+                    + "\nAdd chocolate: " + chocolate
+                    + "\nQuantity: " + quantity
+                    + "\nTotal: Its Free";
         }
         else{
-            double total  = calculatePrice(quantity,3.5);
+            double total  = calculatePrice(quantity,5,wCream,choco);
             DecimalFormat myDecimalFormat = new DecimalFormat("#.00");
 
-            return "Name: Jayson Mallari \nQuantity: "+ quantity + "\nTotal: "+ myDecimalFormat.format(total);
+            return getString(R.string.order_summary_name,name)
+                    + "\nAdd whipped cream: " + whippedCream
+                    + "\nAdd chocolate: " + chocolate
+                    + "\nQuantity: " + quantity
+                    + "\nTotal: "+ myDecimalFormat.format(total);
         }
+
+
+    }
+
+
+    private String subject(String name){
+        return "Just Java order of " + name;
     }
 
     /**
      * This method is called when the order button is clicked.
+     * Display the summary of the order
+     * check if customer wants whippedcream
      */
     public void submitOrder(View view) {
+          EditText nameText  = (EditText) findViewById(R.id.name_field);
+           String name  =  nameText.getText().toString();
 
-            displayText(createOrderSummary());
-            displayTextThankyou();
+           CheckBox whippedCreamAdded = (CheckBox) findViewById(R.id.whipped_cream);
+           Boolean hasWhippedCream = whippedCreamAdded.isChecked();
+
+           CheckBox chocolateAdded = (CheckBox) findViewById(R.id.chocolate);
+           Boolean hasChocolate = chocolateAdded.isChecked();
+
+
+           String output = displayText(createOrderSummary(hasWhippedCream, hasChocolate, name));
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(intent.EXTRA_SUBJECT, subject(name));
+        intent.putExtra(intent.EXTRA_TEXT, output);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
 
     }
 
