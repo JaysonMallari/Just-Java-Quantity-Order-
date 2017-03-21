1995d4e779a005ecfc15680fe8899629f86bae12
renzmallari <renzmallari777@yahoo.com>
2017-03-20 21:25:30 -0400

    features method updated

--- app/src/main/java/com/hmmwow/www/justjava/MainActivity.java
+++ app/src/main/java/com/hmmwow/www/justjava/MainActivity.java
@@ -3,7 +3,7 @@ package com.hmmwow.www.justjava;
 /**
  * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
- *
+ * <p>
  * package com.example.android.justjava;
  */
 
@@ -12,7 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
+import java.text.DecimalFormat;
 
 
 /**
@@ -28,41 +28,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void increment(View view){
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /* INCREMENT */
+    public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
 
     }
 
-    public void decrement(View view){
-        if (quantity > 0){
-            quantity -=1;
+    /* DECREMENT */
+    public void decrement(View view) {
+        if (quantity > 0) {
+            quantity -= 1;
         }
-        display(quantity);
+        displayQuantity(quantity);
 
+    }
+
+
+    /*
+    * THIS METHOD WILL GIVE TXT TO THE SCREEN
+     */
+    private void displayText(String message){
+        TextView orderSummaryTextView  = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView .setText(message);
+    }
+
+    /*
+    *     THANK YOU TEXT VIEW
+     */
+    public void displayTextThankyou(){
+        TextView thankYouMessage = (TextView) findViewById(R.id.thank_you_text_view);
+        thankYouMessage.setText("Thank You!");
+    }
+
+    /**
+     * will calculate the total price
+     * @ return total price
+     */
+    public double calculatePrice(int count, double price){
+        double totalPrice = count * price;
+        return totalPrice;
+    }
+
+    /**
+     *  Summary of the order
+     */
+
+    private String createOrderSummary(){
+        if (quantity < 1){
+            return "Name: Jayson Mallari \nQuantity: "+ quantity + "\nTotal: Its Free";
+        }
+        else{
+            double total  = calculatePrice(quantity,3.5);
+            DecimalFormat myDecimalFormat = new DecimalFormat("#.00");
+
+            return "Name: Jayson Mallari \nQuantity: "+ quantity + "\nTotal: "+ myDecimalFormat.format(total);
+        }
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+
+            displayText(createOrderSummary());
+            displayTextThankyou();
+
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(""+number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
 }
